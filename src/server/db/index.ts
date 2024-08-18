9e938494aebd0dcd9e3db3d2eec3221cbb91a28d
Owen McCormick <owenmccr@gmail.com>
2024-08-18 18:51:31 -0400

    database setup

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,6 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 import postgres from "postgres";
-
+import { sql } from "@vercel/postgres";
 import { env } from "@/env";
 import * as schema from "./schema";
 
@@ -12,7 +12,8 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(conn, { schema });
+// Use this object to send drizzle queries to your DB
+export const db = drizzle(sql, { schema });
