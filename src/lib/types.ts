d1cc19322fedd0a3a43ac30b004dbcf7f74ed7d0
Owen McCormick <owenmccr@gmail.com>
2024-08-28 20:46:00 -0400

    remove unneeded types file

--- src/lib/types.ts
+++
@@ -1,6 +0,0 @@
-// This is a temporary fix for the User typescript error. Waiting for Lucia devs to respond.
-import type { User } from "lucia";
-
-export interface CurrentUser extends User {
-  username: string;
-}
