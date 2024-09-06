4f28ea707b765fa015c471a8f4abf50836d77307
Owen McCormick <owenmccr@gmail.com>
2024-09-05 22:26:33 -0400

    added shadcn dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { api, HydrateClient } from "@/trpc/server";
 import TimerSection from "@/components/timer-section";
 import { validateRequest } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import Dashboard from "@/components/dashboard";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default async function Page() {
   const { user } = await validateRequest();
@@ -15,9 +17,12 @@ export default async function Page() {
   return (
     <HydrateClient>
       <div className="flex min-h-screen flex-col items-center justify-center">
-        <div className="w-6/12">
-          <TimerSection />
-        </div>
+        {/* <div className="w-6/12"> */}
+          {/* <TimerSection /> */}
+          <TooltipProvider>
+            <Dashboard />
+          </TooltipProvider>
+        {/* </div> */}
       </div>
     </HydrateClient>
   );
