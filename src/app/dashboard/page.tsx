653f990c59d54377337b9d0342775269d38a9eb1
Owen McCormick <owenmccr@gmail.com>
2024-09-07 00:22:18 -0400

    some styling and auth changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,22 +2,25 @@ import { api, HydrateClient } from "@/trpc/server";
 import TimerSection from "@/components/timer-section";
 import { validateRequest } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { validateUser } from "@/lib/utils";
 
 export default async function Page() {
-  const { user } = await validateRequest();
+  const user = await validateUser();
 
-  if (!user) {
-    redirect("/login");
-  }
+  console.log(user);
 
   void api.timer.getTimers.prefetch();
 
   return (
     <HydrateClient>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <div className="w-6/12">
-          <TimerSection />
-        </div>
+      <div>
+        <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+        <p className="text-muted-foreground">
+          Here&apos;s a list of your tasks for this month!
+        </p>
+      </div>
+      <div className="w-6/12">
+        <TimerSection />
       </div>
     </HydrateClient>
   );
